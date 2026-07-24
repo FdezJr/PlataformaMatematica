901cdcad7230d300bae98d82810787f9b8a19725
FdezJr <49888602+FdezJr@users.noreply.github.com>
2026-07-24 13:35:50 -0400

    Add Firebase initialization module

---
+++ js/config/firebase.js
@@ -0,0 +1,18 @@
+/**
+ * Módulo de Inicialización de Firebase
+ */
+const firebaseConfig = { 
+    apiKey: "AIzaSyBPvo4UAztPUKREsXaG0dbwKE-nrldlJPk", 
+    authDomain: "tamizajefuncionesejecutivas.firebaseapp.com", 
+    projectId: "tamizajefuncionesejecutivas", 
+    storageBucket: "tamizajefuncionesejecutivas.firebasestorage.app", 
+    messagingSenderId: "730702504761", 
+    appId: "1:730702504761:web:bb21db2ec770e06af4fee8", 
+    measurementId: "G-8ZRJM1XBBZ" 
+};
+
+if (!firebase.apps.length) { 
+    firebase.initializeApp(firebaseConfig); 
+}
+
+export const db = firebase.firestore();
